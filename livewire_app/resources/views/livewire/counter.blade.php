0b4428bd6475f43f3ec509d2cfd9c144bb74e4e9
Davi Gledson <davigledson@outlook.com>
2023-07-12 11:55:01 -0300

    funcao de inclementacao

--- livewire_app/resources/views/livewire/counter.blade.php
+++ livewire_app/resources/views/livewire/counter.blade.php
@@ -1,4 +1,6 @@
 <div>
     {{-- Stop trying to control. --}}
     <h1>oOIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII</h1>
+    <button wire:click="increment">+</button>
+    <h1>{{ $count }}</h1>
 </div>
